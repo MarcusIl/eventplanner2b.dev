c1e103d7fc7145769230221003d1c3068af9368b
Aleksis-t <tukissaleksis@gmail.com>
2023-06-29 20:41:12 +0300

    s

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::group(['middleware' => 'auth'], function(){
+Route::group(['middleware' => 'auth'], function () {
     Route::group([
         'prefix' => 'admin',
         'middleware' => 'is_admin',
         'as' => 'admin',
-    ], function(){
-        Route::get('')
+    ], function () {
+        Route::get('/', function () {
+            // Logic for admin route
+        });
     });
-})
+});
