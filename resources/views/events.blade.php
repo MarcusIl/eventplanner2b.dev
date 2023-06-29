de74d3da6be1f2d99792027350bf5e41d6a09450
Marcus <markussilgazs@gmail.com>
2023-06-29 23:15:10 +0300

    more events

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -1,15 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Events</title>
-</head>
-
-<body>
     <h1>Pick an event you like</h1>
     @if (count($events) == 0)
         <p class='error'>There are no records in the database!</p>
@@ -17,14 +8,11 @@
         <ul>
             @foreach ($events as $event)
                 <li>
-                    {{ $event->name }}
+                    <a href="{{ route('events.show', $event->id) }}">{{ $event->name }}</a>
                 </li>
             @endforeach
         </ul>
     @endif
 
     <a href="{{ route('events.create') }}" class="btn btn-primary">Create Event</a>
-</body>
-
-</html>
 @endsection
