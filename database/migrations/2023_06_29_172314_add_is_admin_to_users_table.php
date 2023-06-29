813e20f4aa1402e18e1ca8a4e4ac3eaad816139d
Marcus <markussilgazs@gmail.com>
2023-06-29 21:20:40 +0300

    add is admin

--- database/migrations/2023_06_29_172314_add_is_admin_to_users_table.php
+++ database/migrations/2023_06_29_172314_add_is_admin_to_users_table.php
@@ -8,13 +8,8 @@ class CreateUsersTable extends Migration
 {
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+        Schema::table('users', function($table){
             $table->boolean('is_admin')->default(false);
-            $table->timestamps();
         });
     }
 
