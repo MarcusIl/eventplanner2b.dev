a9f014eb70b35def0ffc58982ecef9996fc529e2
Marcus <markussilgazs@gmail.com>
2023-06-29 20:28:59 +0300

    trying again

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::group([
+        'prefix' => 'admin',
+        'middleware' => 'is_admin',
+        'as' => 'admin',
+    ], function(){
+        Route::get('')
+    });
+})
