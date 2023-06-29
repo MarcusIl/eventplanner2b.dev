c9626d1f6d13923c8b45e4b4fba25ba0c94b2766
Aleksis-t <tukissaleksis@gmail.com>
2023-06-29 21:50:44 +0300

    ss

--- app/Models/RSVP.php
+++ app/Models/RSVP.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class RSVP extends Model
 {
     use HasFactory;
+
+    protected $table = 'rsvp';
+
+    protected $fillable = [
+        'user_id',
+        'event_id',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    // Add any other custom methods or relationships here
 }
