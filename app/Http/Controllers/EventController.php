6037cb410fa6e12b856f538cdbaa3eb5f4037168
Aleksis-t <tukissaleksis@gmail.com>
2023-06-29 23:57:40 +0300

    s

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,6 +7,7 @@ use App\Mail\EventInvitation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Mail;
 
 
 
