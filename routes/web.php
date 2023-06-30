6a0b972117374436bf3f20f7cda748ecc1e65adf
Aleksis-t <tukissaleksis@gmail.com>
2023-06-30 11:12:50 +0300

    s

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::prefix('admin')->middleware('auth')->group(function(){
 
 Route::get('/events', [EventController::class, 'index'])->name('events.index');
 Route::get('/events/create', [EventController::class, 'showCreateForm'])->name('events.create');
-Route::post('/events', [EventController::class, 'create'])->name('events.store');
+Route::post('/events', [EventController::class, 'store'])->name('events.store');
 Route::get('/events/{event}', [EventController::class, 'show'])->name('events.show');
 Route::get('/events/{event}/invite', [EventController::class, 'invite'])->name('events.invite');
 Route::delete('/events/{event}', [EventController::class, 'destroy'])->name('events.destroy');
